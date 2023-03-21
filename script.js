c13731ece01f889d9583cdd19b1aa0e971c53460
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-03-21 11:32:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 require("ace/ext/emmet");
 var editors = [ace.edit("html"), ace.edit("css"), ace.edit("js")];
 var editorTypes = ["html", "css", "javascript"];
-var result = document.getElementById("result").contentWindow.document;
+var result = document.getElementById("result");
 for(var i = 0; i < 3; i++) {
   editors[i].setTheme("ace/theme/monokai");
   editors[i].session.setMode("ace/mode/" + editorTypes[i]);
   editors[i].session.setUseWorker(false);
   editors[i].setOption("enableEmmet", true);
   editors[i].session.on("change", function() {
-    result.open();
-    result.writeln("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
-    result.close();
+    result.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
   });
 }
 document.getElementById("save").addEventListener("dblclick", function() {
