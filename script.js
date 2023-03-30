21c2329e052238844a736392104ded928be59f62
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-03-30 14:45:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 require("ace/ext/emmet");
 window.addEventListener("beforeunload", function(event) {event.returnValue = true;});
+function gqs() {
+  let qParams = {};
+  let anchor = document.createElement("a");
+  anchor.href = location.href;
+  let qStrings = anchor.search.substring(1);
+  let params = qStrings.split("&");
+  for(let i = 0; i < params.length; i++) {
+      let pair = params[i].split("=");
+      qParams[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return qParams;
+}
 var editors = [ace.edit("html"), ace.edit("css"), ace.edit("js")];
 var editorTypes = ["html", "css", "javascript"];
 var result = document.getElementById("result");
+function showResult() {
+  result.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
+}
 for(var i = 0; i < 3; i++) {
   editors[i].setTheme("ace/theme/monokai");
   editors[i].session.setMode("ace/mode/" + editorTypes[i]);
   editors[i].session.setUseWorker(false);
   editors[i].session.setUseWrapMode(true);
   editors[i].setOption("enableEmmet", true);
-  editors[i].session.on("change", function() {
-    result.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
-  });
+  editors[i].setValue(gqs()[editorTypes[i]]);
+  editors[i].session.on("change", showResult);
 }
+showResult();
 document.getElementById("save").addEventListener("dblclick", function() {
   var title = prompt("Title:");
   if(title !== null) {
