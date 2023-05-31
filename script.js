011e7810d667356d3de175805183b59bf42bfeac
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-05-31 10:51:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 require("ace/ext/emmet");
-function gqs() {
-  let qParams = {};
-  let anchor = document.createElement("a");
-  anchor.href = location.href;
-  let qStrings = anchor.search.substring(1);
-  let params = qStrings.split("&");
-  for(let i = 0; i < params.length; i++) {
-    let pair = params[i].split("=");
-    qParams[pair[0]] = decodeURIComponent(pair[1]);
-  }
-  return qParams;
-}
 var editors = [ace.edit("html"), ace.edit("css"), ace.edit("js")];
 var editorTypes = ["html", "css", "javascript"];
 var result = document.getElementById("result");
@@ -18,12 +6,13 @@ function showResult() {
   result.srcdoc = "<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>";
 }
 for(var i = 0; i < 3; i++) {
+  var qtext = new URLSearchParams(location.search).get(editorTypes[i]);
   editors[i].setTheme("ace/theme/monokai");
   editors[i].session.setMode("ace/mode/" + editorTypes[i]);
   editors[i].session.setUseWorker(false);
   editors[i].session.setUseWrapMode(true);
   editors[i].setOption("enableEmmet", true);
-  editors[i].setValue(gqs()[editorTypes[i]], 1);
+  editors[i].setValue(qtext, 1);
   editors[i].session.on("change", showResult);
 }
 showResult();
@@ -31,7 +20,7 @@ document.getElementById("save").addEventListener("dblclick", function() {
   var title = prompt("Title:");
   if(title !== null) {
     var element = document.createElement("a");
-    element.href = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><title>" + title + "</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
+    element.href = "data:text/html," + encodeURIComponent("<!DOCTYPE html><html><head><title>" + title + "</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + editors[1].getValue() + "</style></head><body>" + editors[0].getValue() + "<script>" + editors[2].getValue() + "<" + "/script></body></html>");
     element.download = "program.html";
     document.body.appendChild(element);
     element.click();
@@ -39,7 +28,7 @@ document.getElementById("save").addEventListener("dblclick", function() {
   }
 });
 document.getElementById("copy-link").addEventListener("click", function() {
-  navigator.clipboard.writeText("file:///Users/Alfred/Desktop/Vinsen/darkside/index.html?html=" + encodeURIComponent(editors[0].getValue()) + "&css=" + encodeURIComponent(editors[1].getValue()) + "&javascript=" + encodeURIComponent(editors[2].getValue()));
+  navigator.clipboard.writeText("https://coders2-hcj.github.io/darkside/index.html" + encodeURIComponent(editors[0].getValue()) + "&css=" + encodeURIComponent(editors[1].getValue()) + "&javascript=" + encodeURIComponent(editors[2].getValue()));
   document.getElementById("copy-link").textContent = "Copied!";
   setTimeout(function() {
     document.getElementById("copy-link").textContent = "Copy Link";
