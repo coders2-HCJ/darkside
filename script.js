8d6555c58490c51dce50403c3a75b4801c175a51
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2024-01-16 16:14:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.getElementById("save").addEventListener("dblclick", function() {
   }
 });
 document.getElementById("copy-link").addEventListener("click", function() {
-  navigator.clipboard.writeText("https://coders2-hcj.github.io/darkside/index.html?html=" + encodeURIComponent(editors[0].getValue()) + "&css=" + encodeURIComponent(editors[1].getValue()) + "&javascript=" + encodeURIComponent(editors[2].getValue()));
+  navigator.clipboard.writeText(`${location.href}?html=${encodeURIComponent(editors[0].getValue())}&css=${encodeURIComponent(editors[1].getValue())}&javascript=${encodeURIComponent(editors[2].getValue())}`);
   document.getElementById("copy-link").textContent = "Copied!";
   setTimeout(function() {
     document.getElementById("copy-link").textContent = "Copy Link";
