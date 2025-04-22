64a2c5f9dbfd334efbf8b0847e039efaeb121a70
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2025-04-23 06:37:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 require("ace/ext/emmet");
+function openTab(event, id) {
+  var tabContents = document.getElementsByClassName("tab-content");
+  for(var i = 0; i < tabContents.length; i++) {
+    tabContents[i].style.display = "none";
+  }
+  var tabs = document.getElementsByClassName("tab");
+  for(var i = 0; i < tabs.length; i++) {
+    tabs[i].classList.remove("active");
+  }
+  document.getElementById(id).style.display = "block";
+  event.currentTarget.classList.add("active");
+}
 var editors = [ace.edit("html"), ace.edit("css"), ace.edit("js")];
 var editorTypes = ["html", "css", "javascript"];
 var result = document.getElementById("result");
